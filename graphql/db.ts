7b4f62bf799b1b931c2f8b4fee6cdc0c87b5b9de
5tarlight <developerstarlight@gmail.com>
2020-12-25 20:37:47 +0900

    add person mutation

--- graphql/db.ts
+++ graphql/db.ts
@@ -5,29 +5,29 @@ interface Person {
   gender: string,
 }
 
-export const people = [
+export let people = [
   {
     id: 0,
     name: 'kks1',
-    age: 17,
+    age: 13,
     gender: 'female'
   },
   {
     id: 1,
     name: 'kks2',
-    age: 17,
+    age: 14,
     gender: 'female'
   },
   {
     id: 2,
     name: 'kks3',
-    age: 17,
+    age: 15,
     gender: 'female'
   },
   {
     id: 3,
     name: 'kks4',
-    age: 17,
+    age: 16,
     gender: 'female'
   },
   {
@@ -39,12 +39,35 @@ export const people = [
   {
     id: 5,
     name: 'kks6',
-    age: 17,
+    age: 18,
     gender: 'female'
   },
 ];
 
 export const getById = (id: number): Person => {
   const filteredPeople = people.filter(person => id === person.id);
-  return filteredPeople[0]
+  return filteredPeople[0];
+}
+
+export const deletePerson = (id: number) => {
+  const cleanedPeople = people.filter(person => person.id !== id);
+
+  if (people.length > cleanedPeople.length) {
+    people = cleanedPeople;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addPerson = (name: string, age: number, gender: string) => {
+  const newPerson = {
+    id: people.length + 1,
+    name,
+    age,
+    gender,
+  };
+
+  people.push(newPerson);
+  return newPerson;
 }
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,10 +1,13 @@
-import { getById, people } from "./db";
+import { addPerson, getById, people } from "./db";
 
 const resolvers = {
   Query: {
     people: () => people,
     person: (_, { id }: { id: number }) => getById(id)
-  }
+  },
+  Mutation: {
+    addPerson: (_, { name, age, gender }: { name: string, age: number, gender: string }) => addPerson(name, age, gender)
+  },
 };
 
 export default resolvers;
